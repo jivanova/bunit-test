338535ff24b81795c0b97d85060f215bcf64e40f
Joana <Joana.Ivanova@telerik.com>
2021-02-03 08:11:50 +0200

    chore: add missing testcontext extension file

---
+++ BUnit_Test/Common/TestContextExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Bunit;
+using BUnit_Sample_App.Components;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BUnit_Test.Common
+{
+    public static class TestContextExtensions
+    {
+        public static void Setup(this MyTestContext ctx)
+        {
+            ctx.PopupContainer = ctx.RenderComponent<PopupContainer>();
+
+            ctx.RenderTree.TryAdd<CascadingValue<PopupContainer>>(p =>
+            {
+                p.Add(parameters => parameters.Name, "Container");
+                p.Add(parameters => parameters.Value, ctx.PopupContainer.Instance);
+            });
+        }
+    }
+
+    public class MyTestContext : TestContext
+    {
+        public IRenderedComponent<PopupContainer> PopupContainer { get; set; }
+    }
+}
